6bc4eccc11c1545bb08da525326afabe333a85b3
zhangfan <1026418670@qq.com>
2020-05-29 18:02:06 +0800

    开发者第二次提交

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World! ¿ª·¢Õß xiangmujingli");
+        System.out.println("kaifazhe commit");
     }
 }
